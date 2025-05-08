2577c3d371a76c666a5a685f52f37b8fcf9ce48b
Li, Sheng-Wei <pha123661@gmail.com>
2025-05-09 01:12:12 +0800

    Add llama-cpp-server serving

---
+++ llama-cpp-server/download_gguf.sh
@@ -0,0 +1,3 @@
+mkdir -p models
+wget https://huggingface.co/liswei/emojilm-0.6b-GGUF/resolve/main/emojilm-0.6b-q8_0.gguf?download=true -O models/emojilm-0.6b-q8_0.gguf
+wget https://huggingface.co/liswei/emojilm-0.6b-GGUF/resolve/main/emojilm-0.6b-f16.gguf?download=true -O models/emojilm-0.6b-f16.gguf
