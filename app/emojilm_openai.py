68348aa9df7c9208d94f7e824620bad1a52a8bb0
Li, Sheng-Wei <pha123661@gmail.com>
2025-05-09 15:48:40 +0800

    Add postprocessing to remove non-emoji characters

--- app/emojilm_openai.py
+++ app/emojilm_openai.py
@@ -6,6 +6,7 @@ from asyncio import Semaphore
 from urllib.parse import urljoin
 
 import aiohttp
+import emoji
 import fasttext
 import nltk
 from async_lru import alru_cache
@@ -162,16 +163,13 @@ def preprocess_input_text(input_text: str):
             cleaned_sentences.append(sentence)
         return cleaned_sentences, delimiter_list
 
-
 def post_process_output(output_emoji: str):
-    if re.match(r"<(.*?)>", output_emoji):
-        try:
-            code_points = re.findall(r"<(.*?)>", output_emoji)
-            output_emoji = bytes(int(code_unit, 16)
-                                 for code_unit in code_points).decode('utf-8')
-        except ValueError:
-            pass
-    return output_emoji
+    ret = ''.join(char for char in output_emoji if emoji.is_emoji(char))
+
+    if output_emoji != ret:
+        logger.warning(f"Model output contains non-emoji: `{output_emoji}` Post Processed: `{ret}`")
+
+    return ret
 
 
 async def main():
@@ -192,6 +190,24 @@ async def main():
 出事了還在那邊嘻嘻哈哈合照
 
 （老爸是長髮男）
+
+12345678
+
+立法院今（9日）三讀通過國民黨立法院黨團與民眾黨立法院黨團版「紀念日及節日實施條例」草案，該案被在野稱為「還假於民」法案。此次新增教師節、光復節、行憲紀念日（同日也是聖誕節）、小年夜等4天休假一天，勞動假從原本僅勞工族群放假一日，改為全國性休假，意即所謂的「4+1」休假。
+
+國民黨立委賴士葆、許宇甄分別擬具「紀念日及節日實施法草案」、藍委牛煦庭、林思銘、葉元之等、民眾黨立法院黨團分別擬具「紀念日及節日實施條例草案」等多項相關草案，經內政委員會協商後，交付由韓國瑜召集的朝野黨團協商，但昨天朝野黨團協商就此並無共識便全案交付二讀。國民黨團與民眾黨9日針對「紀念日及節日實施條例」草案共提出再修正動議。
+
+立法院9日下午1時半許進行逐條表決，全案結果皆為出席110位委員，贊成59位、反對51位，贊成者多數通過。經藍白黨團提議進行三讀，在議事人員宣讀全案後，進行全案表決。立法院長韓國瑜宣告，出席107位委員，贊成57位、反對50位，贊成者多數通過，全案表決通過，「紀念日及節日實施條例」制訂通過。
+
+該案指出，為彰顯紀念日及節日的特殊意義，並規定放假日期及舉行慶祝或紀念活動，以紀念國家發展歷史、傳承各族群傳統民俗及促進多元文化發展，特制定本條例。 根據該條例第4條，孔子誕辰紀念日（教師節，9月28日）、台灣光復暨金門古寧頭大捷紀念日（光復節，10月25日）、行憲紀念日（12月25日）列放假一天。
+
+條例第6條第1項規定，「除夕及春節：自農曆十二月末日之前一日至翌年一月三日，放假五日」；換句話說，「小年夜」被列入假日。根據同條例第2項，勞動節改為全國放假。另外，同條例第3項也寫明，「原住民族歲時祭儀：由原住民依其族別歲時祭儀擇定三日放假」，代表原住民族歲時祭儀從1天增至3天。
+
+條例第9條提及，由於上述第4、6條等放假日調整，「交通運輸、警察、消防、海巡、醫療、關務、矯正等全年無休實施輪班、輪休制度之政府機關或機構，由目的事業主管機關調移之」、「軍事機關基於國防安全考量及因應戰備之需要，由目的事業主管機關視實際需要調移之」。
+
+國民黨團表示，這次「還假於民」的法案，是國民黨團在年初定調的優先民生法案，既然答應要還給勞工國定假日，就必須說到做到。本次修法增加的國定假日不只可以給予勞工更多的休息時間，降低工時、減緩血汗，有更多的時間能夠陪伴家人，想要有更多收入的勞工也能因此獲得雙倍工資。對勞工而言，只贏不輸。
+
+國民黨團說，紀念日及節日實施條例草案三讀，不只是履行國民黨對勞工的承諾，也是國民黨展現，當民進黨傾全國全黨之力在做政治鬥爭、撕裂社會、詐欺治國，國民黨仍然無所畏懼，堅持推動民生議題，堅持做對的事情，為人民的福利而努力。
 """
     output, output_emoji_set = await emoji_lm.generate(input_text)
     print(f"Output: {output}")
